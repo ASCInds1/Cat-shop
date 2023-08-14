0c6b6800314ef93351115561f63ace0fea4f3c16
ASCInds1 <akulashivacharan2004@gmail.com>
2023-08-14 13:37:12 +0100

    Added Tests to The catalogue

---
+++ catalogue/BasketTest.java
@@ -0,0 +1,77 @@
+package catalogue;
+
+import middle.StockException;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import middle.StockException;
+
+public class BasketTest {
+
+    Basket basket = new Basket();
+    Product p1 = new Product("0001", "40 inch tv", 200.00, 1 );
+
+    // Testing that the basket has been created, if so it will be not null.
+    @Test
+    public void testBasket() {
+        assertNotNull(basket);
+    }
+
+    // Checking that the setorderNumber() method works as expected.
+    @Test
+    public void testSetOrderNum() {
+        basket.setOrderNum(4);
+        assertEquals(4, basket.getOrderNum());
+    }
+
+    // Checking that GetOrderNum() method works as expected.
+    @Test
+    public void testGetOrderNum() {
+        basket.setOrderNum(4);
+        assertEquals(4, basket.getOrderNum());
+    }
+
+    // Checking that GetSavings() method works as expected.
+
+
+    // Checking that GetTotalPrice() method works as expected.
+
+
+    // Checking that setTotalPrice() method works as expected.
+
+
+    // Checking that adding items to the basket works as expected.
+    @Test
+    public void testAddProduct() {
+        basket.add(p1);
+        if(basket.contains(p1)) {
+            assert(true);
+        }else {
+            assert(false);
+        }
+    }
+
+    // Checking that the discount button applies a discount.
+
+
+    // Checking that getDetails() Method works as expected.
+    @Test
+    public void testGetDetails() {
+        basket.getDetails();
+    }
+
+    // Checking that the removeItem() method works as expected.
+
+}
+
+
+
+
---
+++ catalogue/BetterBasketTest.java
@@ -0,0 +1,49 @@
+package catalogue;
+
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BetterBasketTest {
+    private BetterBasket betterBasket;
+
+    @Before
+    public void setUp() {
+        betterBasket = new BetterBasket();
+    }
+
+    @Test
+    public void testAddNewProduct() {
+        Product newProduct = new Product("P123", "Sample Product", 10.0, 2);
+        assertTrue(betterBasket.add(newProduct));
+        assertEquals(1, betterBasket.size());
+    }
+
+    @Test
+    public void testAddExistingProduct() {
+        Product existingProduct = new Product("P123", "Existing Product", 15.0, 3);
+        betterBasket.add(existingProduct);
+
+        Product newProduct = new Product("P123", "Sample Product", 10.0, 2);
+        assertTrue(betterBasket.add(newProduct));
+
+        assertEquals(1, betterBasket.size());
+        assertEquals(5, existingProduct.getQuantity()); // Quantity should be updated
+    }
+
+    @Test
+    public void testAddMultipleProducts() {
+        Product product1 = new Product("P123", "Product 1", 10.0, 2);
+        Product product2 = new Product("P456", "Product 2", 20.0, 3);
+        Product product3 = new Product("P789", "Product 3", 5.0, 1);
+
+        assertTrue(betterBasket.add(product1));
+        assertTrue(betterBasket.add(product2));
+        assertTrue(betterBasket.add(product3));
+
+        assertEquals(3, betterBasket.size());
+    }
+}
+
